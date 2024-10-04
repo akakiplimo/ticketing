c9d921ff3f50aa82adb20a138c663b35605fe1f8
abracodeabra <akakiplimo@gmail.com>
2024-10-04 11:12:51 +0300

    Response Normalization Strategies i.e. error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,24 +1,32 @@
-import express, {Request, Response} from "express";
-import { body } from "express-validator";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
-router.post("/api/users/signup", [
-  body('email')
-    .isEmail()
-    .withMessage('Email must be valid!'),
-  body('password')
-    .trim()
-    .isLength({min: 4, max: 20})
-    .withMessage('Password must have between 4 and 20 characters')
-], (req: Request, res: Response) => {
-  const { email, password } = req.body;
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid!"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must have between 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-  if (!email || typeof email !== 'string') {
-    res.status(400).send('Provide a valid email!')
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array())
+    }
+
+    console.log("Creating a user...");
+
+    throw new DatabaseConnectionError();
+
+    res.send({});
   }
- 
-  res.send("HI there!");
-});
+);
 
 export { router as signUpRouter };
