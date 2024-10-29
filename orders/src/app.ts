1e95a7d42d81f1ed23687fcf5a1b13e01d351b4f
abracodeabra <akakiplimo@gmail.com>
2024-10-29 11:27:00 +0300

    Scaffold the Orders Service

---
+++ orders/src/app.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@abracodeabra-tickets/common";
+import { newOrderRouter } from "./routes/new";
+import { deleteOrderRouter } from "./routes/delete";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    // no need to sign cookie as it will bring problems with microservice arch
+    // & jwt is inherently secure as signature can be verified
+    signed: false,
+    // only allow https (secure connections)
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+
+app.use(newOrderRouter);
+app.use(deleteOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
